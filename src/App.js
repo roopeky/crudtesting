596be576edf3ec773559ae9113f5773d826fa044
roopeky <roope.kylli@metropolia.fi>
2022-11-21 12:27:02 +0200

    ye buddy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React from "react";
 
-function App() {
+const baseURL = "https://jsonplaceholder.typicode.com/posts";
+
+export default function App() {
+  const [post, setPost] = React.useState(null);
+  const [error, setError] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  function createPost() {
+    axios
+      .post(baseURL, {
+        title: "Hello World!",
+        body: "This is a new post.",
+      })
+      .then((response) => {
+        setPost(response.data);
+      });
+  }
+
+  function updatePost() {
+    axios
+      .put(`${baseURL}/1`, {
+        title: "Hello World!",
+        body: "This is an updated post.",
+      })
+      .then((response) => {
+        setPost(response.data);
+      });
+  }
+
+  function deletePost() {
+    axios.delete(`${baseURL}/1`).then(() => {
+      alert("Post deleted!");
+      setPost(null);
+    });
+  }
+
+  if (error) return `Error: ${error.message}`;
+  if (!post) return "No post!";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+      <button onClick={createPost}>Create Post</button>
+      <button onClick={updatePost}>Update Post</button>
+      <button onClick={deletePost}>Delete Post</button>
     </div>
   );
 }
-
-export default App;
